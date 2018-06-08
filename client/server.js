1eed287600cfe000e6f3a176be21b03d9cb5eede
kamil.lolo@mail.com <Zi0mal15>
2018-06-08 23:19:10 +0200

    Konfiguracja pod Heroku

--- client/server.js
+++ client/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port =  process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/build'));
 app.get('/test', (req, res) => res.send('Heroku Demo!'));
